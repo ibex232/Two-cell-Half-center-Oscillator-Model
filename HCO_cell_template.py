bdb6848c3fb87af5d24720de970b848e7240d0cf
Zach Noah <znoah232@gmail.com>
2025-11-05 17:58:31 -0600

    Middle-ground ELeak

--- HCO_cell_template.py
+++ HCO_cell_template.py
@@ -24,7 +24,7 @@ class HCOCellTemplate(object):
     self.soma.cm = 1
 
     self.soma.insert('leak')
-    self.soma.eleak = -55
+    self.soma.eleak = -65
 
     self.soma.insert('na')
     self.soma.ena = 50 # This is just taken from his rn,, check its right
